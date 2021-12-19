95ee384a9f7508fd93c6a8ed6a4e4bd62f17d9a6
kretu <krecik@gmail.com>
2021-12-19 18:32:09 +0100

    x

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,18 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h> 
 
-int R_Led_pin = 12;
-int G_Led_pin = 12;
-int B_Led_pin = 12;
+#define R_Led_pin 16
+#define G_Led_pin 12
+#define B_Led_pin 14
+#define IGN_pin 15
+#define RX1_pin 13
+#define RX2_pin 2
 
-float Sender=12; // Sender ID 10 for TX 11 for RX1 12 for RX2
-float IGN_Status_1=0;
-float IGN_Status_2=0;
-float IGN_FIRE_1=0;
-float IGN_FIRE_2=0;
-float safety_word=6969; 
+#define ID 12 // Sender ID 10 for TX 11 for RX1 12 for RX2
+int IGN_Status_1=0; // 0 - disconected 1-no igniter 2-Ready unarmed 3-Ready ARMED
+int IGN_Status_2=0;
+int IGN_FIRE_1=0;
+int IGN_FIRE_2=0;
 
 
 #define SEND_TIMEOUT 100 
@@ -23,13 +25,12 @@ volatile boolean callbackCalled;
 //uint8_t remoteMac_1[] = {0xA0, 0x20, 0xA6, 0x13, 0x60, 0xA8};
 uint8_t remoteMac_1[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 uint8_t remoteMac_2[] = {0xA0, 0x20, 0xA6, 0x13, 0x60, 0xA8};
-
+char macBuf[18];
 
 struct __attribute__((packed)) MESSAGE {
-  float Sender; // Sender ID 10 for TX 11 for RX1 12 for RX2
-  float IGN_Status;
-  float IGN_FIRE;
-  float safety_word; 
+  int Sender = ID; // Sender ID 10 for TX 11 for RX1 12 for RX2
+  int IGN_Status = 0;
+  int IGN_FIRE = 0;
 } messageData;
 
 
@@ -40,6 +41,14 @@ void formatMacAddress(const uint8_t *macAddr, char *buffer, int maxLength)
   snprintf(buffer, maxLength, "%02x:%02x:%02x:%02x:%02x:%02x", macAddr[0], macAddr[1], macAddr[2], macAddr[3], macAddr[4], macAddr[5]);
 }
 
+void LED(int R, int G, int B) {
+  analogWrite(R_Led_pin, R);
+  analogWrite(G_Led_pin, G);
+  analogWrite(B_Led_pin, B);
+}
+
+
+  
 
 
 void initEspNow() {
@@ -61,7 +70,6 @@ void initEspNow() {
     Serial.print("recv_cb, msg from device: "); 
     Serial.println(macStr);
 
-    if (messageData.safety_word==safety_word) {
       if(messageData.Sender==11) {
         Serial.print("Odbiornik 1 Status: ");
         Serial.print(messageData.IGN_Status);
@@ -72,10 +80,9 @@ void initEspNow() {
         Serial.print(messageData.IGN_Status);
         IGN_Status_2=messageData.IGN_Status;
       }
-    }
-    else {
-      Serial.println("Wrong safety word");
-    }
+
+
+    
   });
 
   esp_now_register_send_cb([](uint8_t* mac, uint8_t sendStatus) {
@@ -86,6 +93,7 @@ void initEspNow() {
 
 
 void sendAll() {
+  messageData.Sender = ID;
 uint8_t bs[sizeof(messageData)];
   memcpy(bs, &messageData, sizeof(messageData));
   esp_now_send(NULL, bs, sizeof(messageData)); // NULL means send to all peers
@@ -93,9 +101,10 @@ uint8_t bs[sizeof(messageData)];
 } 
 
 void sendTo(uint8_t* mac) {
+  messageData.Sender = ID;
 uint8_t bs[sizeof(messageData)];
   memcpy(bs, &messageData, sizeof(messageData));
-  esp_now_send(mac, bs, sizeof(messageData)); // NULL means send to all peers
+  esp_now_send(mac, bs, sizeof(messageData)); 
 
 } 
 
@@ -104,24 +113,26 @@ void setup() {
   delay(100);
   
   //PIN init
+  pinMode(IGN_pin, INPUT);
+  pinMode(RX1_pin, INPUT_PULLUP);
+  pinMode(RX2_pin, INPUT_PULLUP);
   pinMode(R_Led_pin, OUTPUT);
-  analogWrite(R_Led_pin, 0);
   pinMode(G_Led_pin, OUTPUT);
-  analogWrite(G_Led_pin, 255);
   pinMode(B_Led_pin, OUTPUT);
-  analogWrite(B_Led_pin, 255);
-  Serial.begin(115200);
+  LED(0,0,0);
+   Serial.begin(115200);
 
   //ESP-NOW Init
 
   WiFi.mode(WIFI_STA); // Station mode for esp-now sensor node
   WiFi.disconnect();
 
-  Serial.printf("This mac: %s, ", WiFi.macAddress().c_str()); 
-  Serial.printf("target mac 1: %02x%02x%02x%02x%02x%02x", remoteMac_1[0], remoteMac_1[1], remoteMac_1[2], remoteMac_1[3], remoteMac_1[4], remoteMac_1[5]); 
-  Serial.printf("target mac 2: %02x%02x%02x%02x%02x%02x", remoteMac_2[0], remoteMac_2[1], remoteMac_2[2], remoteMac_2[3], remoteMac_2[4], remoteMac_2[5]); 
+  Serial.printf("This mac: %s, \n", WiFi.macAddress().c_str()); 
+  Serial.printf("target mac 1: %02x%02x%02x%02x%02x%02x\n", remoteMac_1[0], remoteMac_1[1], remoteMac_1[2], remoteMac_1[3], remoteMac_1[4], remoteMac_1[5]); 
+  Serial.printf("target mac 2: %02x%02x%02x%02x%02x%02x\n", remoteMac_2[0], remoteMac_2[1], remoteMac_2[2], remoteMac_2[3], remoteMac_2[4], remoteMac_2[5]); 
   Serial.printf(", channel: %i\n", WIFI_CHANNEL); 
-
+  formatMacAddress(remoteMac_1, macBuf, 18);
+  Serial.println(macBuf);
   initEspNow();
 
   
@@ -133,11 +144,27 @@ void setup() {
 
 
 void loop() {
+  if(digitalRead(RX2_pin)||digitalRead(RX1_pin)) {
+    LED(0,255,255);
+  }
+  if(!digitalRead(RX1_pin)) {
+    messageData.IGN_Status = 1;
+    sendAll();
+    LED(255,0,255);
+  }
 
+  if(!digitalRead(RX2_pin)) {
+    messageData.IGN_Status = 2;
+    sendTo(remoteMac_1);
+    LED(255,255,0);
+  }
 
-
-
-
+  if(digitalRead(IGN_pin)) {
+    messageData.IGN_Status = 3;
+    sendTo(remoteMac_2);
+    LED(255,0,0);
+  }
+  delay(500);
 }
 
 
